abb043f484ad514ecbcc64a39e6de7be211bf152
Jorge Jaular Lasaga <jordijaulas13@gmail.com>
2018-02-01 17:36:09 +0100

    El método getLot ha sido modificado para que no dependa de que un lote con número concreto de la colección esté almacenado en el índice (number - 1) de la colección

--- Auction.java
+++ Auction.java
@@ -78,26 +78,30 @@ public class Auction
      */
     public Lot getLot(int lotNumber)
     {
-        if((lotNumber >= 1) && (lotNumber < nextLotNumber)) {
-            // The number seems to be reasonable.
-            Lot selectedLot = lots.get(lotNumber - 1);
-            // Include a confidence check to be sure we have the
-            // right lot.
-            if(selectedLot.getNumber() != lotNumber) {
-                System.out.println("Internal error: Lot number " +
-                                   selectedLot.getNumber() +
-                                   " was returned instead of " +
-                                   lotNumber);
-                // Don't return an invalid lot.
-                selectedLot = null;
+        Lot selectedLot = lots.get(lotNumber - 1);
+        boolean salirDelBucle = false;
+        while (lotNumber - 1 <= lots.size() && !salirDelBucle)
+        {
+            if((lotNumber >= 1) && (lotNumber < nextLotNumber)) {
+                if(selectedLot.getNumber() != lotNumber) {
+                    System.out.println("Internal error: Lot number " +
+                                       selectedLot.getNumber() +
+                                       " was returned instead of " +
+                                       lotNumber);
+                    // Don't return an invalid lot.
+                    selectedLot = null;
+                    salirDelBucle = true;
+                }  
+                salirDelBucle = true;
             }
-            return selectedLot;
+            else {
+                System.out.println("Lot number: " + lotNumber +
+                                   " does not exist.");
+                selectedLot = null;
+                salirDelBucle = true;
+            }
         }
-        else {
-            System.out.println("Lot number: " + lotNumber +
-                               " does not exist.");
-            return null;
-        }
+        return selectedLot;
     }
     
     /**
